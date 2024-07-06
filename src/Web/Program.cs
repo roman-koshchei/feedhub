3edb7c15d03c9e25c2ad9c613a9cc46315d45f97
Roman Koshchei <romankoshchei@gmail.com>
2024-07-06 09:02:58 +0300

    refactor stream slug

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,11 +1,13 @@
 using Lib;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Octokit;
 using System.Diagnostics;
 using System.Text;
 using Web.Controllers;
 using Web.Data;
+using Web.Routes;
 using Web.Services;
 
 Env.LoadFile("./.env");
@@ -97,72 +99,69 @@ app.MapGet("/stream2", async (HttpResponse res) =>
     await res.BodyWriter.FlushAsync();
 });
 
-app.MapGet("/stream/{slug}",
-async (
-    HttpResponse res,
-   [FromServices] Db db,
-   [FromRoute] string slug
-) =>
-{
-    var app = db.Apps.FirstOrDefault(x => x.Slug == slug);
-    if (app != null)
-    {
-        GitHub gitHub = new(app.RepositoryOwner, app.RepositoryName);
-        var issuesTask = gitHub.GetFeedHubIssues();
-
-        await WriteToRes(res, $@"
-            <!DOCTYPE html>
-            <html lang=""en"">
-            <head>
-                <meta charset=""utf-8"" />
-                <meta name=""viewport"" content=""width=device-width, initial-scale=1.0"" />
-                <script defer src=""/lib/htmx.min.js"" asp-append-version=""true""></script>
-                <title>{app.Slug}</title>
-            </head>
-            <body style='overflow: scroll;'>
-                <main role=""main"" style=""max-width:720px; margin-left:auto; margin-right:auto;"">
-                    <div class=""text-center"">
-                        <h1 class=""display-4"">Feedhub</h1>
-                        <p>Leave your Feedback right here, so we know what the heck in your mind:</p>
-                    </div>
-
-                    <form hx-post=""/{app.Slug}"" hx-target=""#list"" hx-swap=""afterbegin"" style=""max-width:360px; width:100%;"">
-                        <input type=""text"" style=""width:100%;"" name=""{nameof(FeedbackController.Input.User)}"" value="""" placeholder=""Name"" />
-                        <textarea style=""width:100%; margin-top:0.5rem;"" rows=""10"" name=""{nameof(FeedbackController.Input.Content)}"" placeholder=""Your feedback""></textarea>
-
-                        <div style=""display:flex; gap:0.5rem; margin-top:0.5rem;"">
-                            <button type=""submit"" value=""{FeedbackController.ReportBugAction}"" name=""action"">Report Bug</button>
-                            <button type=""submit"" value=""{FeedbackController.LeaveFeedbackAction}"" name=""action"">Leave Feedback</button>
-                        </div>
-                    </form>
-                    <div id=""list"">
-        ");
-        await res.BodyWriter.FlushAsync();
-
-        var issues = await issuesTask;
-
-        if (issues != null)
-        {
-            foreach (var issue in issues)
-            {
-                await WriteToRes(res, $@"
-                    <div>
-                        <h3>{issue.Title}</h3>
-                        <p>{issue.Body}</p>
-                    </div>
-                ");
-                await res.BodyWriter.FlushAsync();
-            }
-        }
-
-        await WriteToRes(res, $@"</div></main></body></html>");
-    }
-});
+StreamRoutes.Map(app);
 
 app.Run();
 
-static async Task WriteToRes(HttpResponse res, string content)
+//var c = Component.Div.Style("overflow:hidden;")[
+//    Component.P["very strange syntax"],
+//    Component.Div[
+//        Component.Slot
+//    ],
+//    Component.P["after slot"]
+//];
+
+//var Component = El.Div.Style("overflow.hidden")[
+//    El.P("aaaa"),
+//    El.P("aaaa").Style("color:red;"),
+//    El.Slot(),
+//    El.A(),
+//];
+
+//Component(
+//    IntoSlot(
+
+//    )
+//);
+
+//Component.Start(res);
+
+//Component.End(res);
+
+//var component = Component.Div<T>[
+//    Component.P[(props) => props.Slug],
+//    Component.Slot[
+//        () =>
+//        {
+//            var issues = await issuesTask;
+
+//            if (issues != null)
+//            {
+//                foreach (var issue in issues)
+//                {
+//                    await WriteToRes(res, Component.P[issue.Title, issue.Body]);
+//                    await res.BodyWriter.FlushAsync();
+//                }
+//            }
+//        }
+//]
+//];
+internal class Component
 {
-    var bytes = Encoding.UTF8.GetBytes(content);
-    await res.BodyWriter.WriteAsync(bytes);
+    public readonly Dictionary<string, string> Attributes = [];
+
+    public Component Style(string value)
+    {
+        Attributes.Add("style", value);
+        return this;
+    }
+
+    public Component this[params Component[] component] => this;
+    public Component this[string text] => new();
+
+    public static Component Slot => new();
+
+    public static Component P => new();
+
+    public static Component Div => new();
 }
---
+++ src/Web/Routes/StreamRoutes.cs
@@ -0,0 +1,92 @@
+﻿using System.IO.Pipelines;
+using System.Text;
+using Web.Controllers;
+using Web.Data;
+using Web.Services;
+
+namespace Web.Routes;
+
+public static class StreamRoutes
+{
+    public static (string, string) PageView(string slug)
+    {
+        return ($@"
+            <!DOCTYPE html>
+            <html lang=""en"">
+            <head>
+                <meta charset=""utf-8"" />
+                <meta name=""viewport"" content=""width=device-width, initial-scale=1.0"" />
+                <script defer src=""/lib/htmx.min.js"" asp-append-version=""true""></script>
+                <title>{slug}</title>
+            </head>
+            <body style='overflow: auto scroll;'>
+                <main role=""main"" style=""max-width:720px; margin-left:auto; margin-right:auto;"">
+                    <div class=""text-center"">
+                        <h1 class=""display-4"">Feedhub</h1>
+                        <p>Leave your Feedback right here, so we know what the heck in your mind:</p>
+                    </div>
+
+                    <form hx-post=""/{slug}"" hx-target=""#list"" hx-swap=""afterbegin"" style=""max-width:360px; width:100%;"">
+                        <input type=""text"" style=""width:100%;"" name=""{nameof(FeedbackController.Input.User)}"" value="""" placeholder=""Name"" />
+                        <textarea style=""width:100%; margin-top:0.5rem;"" rows=""10"" name=""{nameof(FeedbackController.Input.Content)}"" placeholder=""Your feedback""></textarea>
+
+                        <div style=""display:flex; gap:0.5rem; margin-top:0.5rem;"">
+                            <button type=""submit"" value=""{FeedbackController.ReportBugAction}"" name=""action"">Report Bug</button>
+                            <button type=""submit"" value=""{FeedbackController.LeaveFeedbackAction}"" name=""action"">Leave Feedback</button>
+                        </div>
+                    </form>
+                    <div id=""list"">", @"</div>
+                </main>
+            </body>
+            </html>");
+    }
+
+    public static string FeedbackView(string title, string content)
+    {
+        return $"<div><h3>{title}</h3><p>{content}</p></div>";
+    }
+
+    public static void Map(IEndpointRouteBuilder builder)
+    {
+        builder.MapGet("/stream/{slug}",
+        async (
+            HttpResponse res, Db db, string slug
+        ) =>
+        {
+            var app = db.Apps.FirstOrDefault(x => x.Slug == slug);
+            if (app != null)
+            {
+                GitHub gitHub = new(app.RepositoryOwner, app.RepositoryName);
+                var issuesTask = gitHub.GetFeedHubIssues();
+
+                var (pageStart, pageEnd) = PageView(app.Slug);
+
+                await res.BodyWriter.WriteString(pageStart);
+                await res.BodyWriter.FlushAsync();
+
+                var issues = await issuesTask;
+                if (issues != null)
+                {
+                    foreach (var issue in issues)
+                    {
+                        await res.BodyWriter.WriteString(FeedbackView(issue.Title, issue.Body));
+                    }
+                    // flus after all issues, because we don't stream issues for now
+                    //await res.BodyWriter.FlushAsync();
+                }
+
+                await res.BodyWriter.WriteString(pageEnd);
+                await res.BodyWriter.CompleteAsync();
+            }
+        });
+    }
+}
+
+public static class BodyWriterExtensions
+{
+    public static async Task WriteString(this PipeWriter writer, string content)
+    {
+        var bytes = Encoding.UTF8.GetBytes(content);
+        await writer.WriteAsync(bytes);
+    }
+}
